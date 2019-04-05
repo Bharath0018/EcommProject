74606c8b57ebfd9ed1a65bbfc73d8df62e0d917f
Bharath0018 <>
2019-04-05 14:24:24 +0530

    GadgetsWorldBackend
    1.Model Class
    2.DAO,DAOImpl

---
+++ GadgetsWorldBackend/src/main/java/com/ecomm/GadgetsWorldBackend/App.java
@@ -0,0 +1,13 @@
+package com.ecomm.GadgetsWorldBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
